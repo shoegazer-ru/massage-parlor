509e00089d88fa84216a51824de6c9f4896597ac
shoegazer <gss@apri-code.ru>
2022-11-18 22:27:44 +0500

    Верстка

--- resources/views/frontend/layouts/base.blade.php
+++ resources/views/frontend/layouts/base.blade.php
@@ -7,78 +7,52 @@
 
     <link rel="stylesheet" href="{{ asset('css/themes/base/base/theme.css') }}">
 
-    <title>Website</title>
+    <title>Массажный салон</title>
 </head>
 
 <body>
-    <header class="page-header">
-        <div class="page-limiter">
-            <div class="header-logo">
-                <a href="/" class="logo"></a>
-            </div>
-            <div class="header-menu">
-                <a href="">Типажи</a>
-                <a href="">Гардероб</a>
-                <a href="">Статьи</a>
-                <a href="">Форум</a>
-            </div>
-            <div class="header-userbar">
-                <a href="" class="userbar-logo"></a>
-            </div>
-        </div>
-    </header>
     <main class="page-content">
         @yield('content')
     </main>
 
-    <footer class="page-footer">
-        <div class="page-limiter">
-            <div class="footer-left">
-                <a href="/" class="footer-logo"></a>
-                <div class="footer-social-links">
-                    <a href="" class="social-link link-instagram">
-                        <span class="link-icon"></span>
-                    </a>
-                    <a href="" class="social-link link-youtube">
-                        <span class="link-icon"></span>
-                    </a>
-                </div>
-            </div>
-            <div class="footer-center">
-                <div class="footer-menu">
-                    <a href="">О проекте</a>
-                    <a href="">Примерка</a>
-                    <a href="">Онлайн-тест Кибби</a>
-                    <a href="">Цветотип в 2 клика</a>
-                </div>
-                <div class="footer-menu">
-                    <a href="">Описание типажей</a>
-                    <a href="">Форум</a>
-                    <a href="">Статьи</a>
-                    <a href="">Гардеробная</a>
-                </div>
-            </div>
-            <div class="footer-right">
-                <div class="footer-feedback">
-                    <p>Обратная связь</p>
-                    <p>Здравствуйте! Подскажите, как я могу получить консультацию? есть номер?</p>
-                    <form action="" method="post">
-                        <input type="text" placeholder="example@mail.ru">
-                        <button><div class="button-icon"></div></button>
-                    </form>
-                </div>
-            </div>
+    <header class="page-header">
+        <div class="header-image">
+            <img src="/images/default-banner.webp" alt="">
         </div>
-        <div class="footer-bottom">
+        <div class="header-overflow">
             <div class="page-limiter">
-                <div class="bottom-left">
-                    <p>© 2021 Kibbe_russia. Все права защищены.</p>
+                <div class="header-top">
+                    <div class="header-social">
+                        <a href="" class="link-item facebook"></a>
+                        <a href="" class="link-item vkontakte"></a>
+                        <a href="" class="link-item instagram"></a>
+                    </div>
+                    <div class="header-logo">
+                        <a href="/" class="logo-image">Massage Parlor</a>
+                    </div>
+                    <div class="header-links">
+                        <a href="" class="call-me-link">Обратный звонок</a>
+                        <a href="" class="appointment-link">Записаться</a>
+                    </div>
                 </div>
-                <div class="bottom-right">
-                    <a href="">Политика конфиденциальности</a>
+                <div class="header-delim"></div>
+                <div class="header-bottom">
+                    <div class="header-menu">
+                        <a href="">Главная</a>
+                        <a href="">Услуги</a>
+                        <a href="">Продукция</a>
+                        <a href="">Доставка</a>
+                        <a href="">Отзывы</a>
+                        <a href="">Команда</a>
+                        <a href="">Контакты</a>
+                    </div>
                 </div>
             </div>
         </div>
+    </header>
+
+    <footer class="page-footer">
+        
     </footer>
 
     <script src="{{ asset('js/themes/base/base/main.js') }}"></script>
